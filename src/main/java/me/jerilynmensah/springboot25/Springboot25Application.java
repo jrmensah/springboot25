9225fde049fc5867e3d8924659fc1bae0841a104
jrmensah <jmensah4@student.umuc.edu>
2017-08-25 16:51:36 -0400

    Deploy with Heroku

---
+++ src/main/java/me/jerilynmensah/springboot25/Springboot25Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.springboot25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot25Application.class, args);
+	}
+}
